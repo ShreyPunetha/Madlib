36c674e0162bce4f7413a944dbda42801e47429a
ShreyPunetha <54269879+ShreyPunetha@users.noreply.github.com>
2024-07-08 22:16:07 -0400

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,21 @@
+# string concatenation 
+
+youtuber = "Shrey Punetha" # some string variable 
+
+print("suscribe to " + youtuber)
+print("suscribe to {}".format(youtuber))
+print(f"suscribe to {youtuber}")
+
+adj = input("Adjective:")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Fampus Person : ")
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited all the time because \ I love to {verb1}. Stay hydrated and {verb2} like your are {famous_person}!"
+
+print(madlib)
+
+
+
+
+
